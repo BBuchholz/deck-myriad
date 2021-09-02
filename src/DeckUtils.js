a15b04961816d980764f8139d4eb6d80342598be
ENTOMDT <entomdt@gmail.com>
2021-09-02 18:42:43 -0500

    Add expected scenarios

---
+++ src/DeckUtils.js
@@ -0,0 +1,160 @@
+const DeckUtils = () => {
+
+  const self = {
+    
+  };
+
+  const deckCapabilities = self => ({
+    
+    getScenarios: () => {
+
+      // You can add new scenarios, but make sure that there is exactly...
+      // * 1 daemonCard per scenario
+      // * 3 playerCards per scenario
+
+      var scenarios = [
+        {
+          daemonCard : {
+            description : "Four of Disks",
+            power : "4D",
+          },
+          playerCards : [
+            {
+              description : "Five of Wands",
+              power : "5W",
+            },
+            {
+              description : "Three of Cups",
+              power : "3C",
+            },
+            {
+              description : "Two of Swords",
+              power : "2S",
+            }
+          ]
+        },
+        {
+          daemonCard : {
+            description : "Six of Disks",
+            power : "6D",
+          },
+          playerCards : [
+            {
+              description : "Five of Swords",
+              power : "5S",
+            },
+            {
+              description : "Three of Disks",
+              power : "3D",
+            },
+            {
+              description : "Two of Wands",
+              power : "2W",
+            }
+          ]
+        },
+        {
+          daemonCard : {
+            description : "Nine of Disks",
+            power : "9D",
+          },
+          playerCards : [
+            {
+              description : "Six of Wands",
+              power : "6W",
+            },
+            {
+              description : "Eight of Cups",
+              power : "8C",
+            },
+            {
+              description : "Four of Swords",
+              power : "4S",
+            }
+          ]
+        },
+        {
+          daemonCard : {
+            description : "Five of Cups",
+            power : "5C",
+          },
+          playerCards : [
+            {
+              description : "Eight of Disks",
+              power : "8D",
+            },
+            {
+              description : "Three of Swords",
+              power : "3S",
+            },
+            {
+              description : "Two of Disks",
+              power : "2D",
+            }
+          ]
+        }
+      ];
+
+      return scenarios;
+    },
+
+    getHDeltaFromSuit: (suit) => {
+    
+      switch (suit) {      
+
+        case 'D':
+          // console.log("disks found");
+          return -1;
+       
+        case 'C':
+          // console.log("cups found");
+          return -1;
+       
+        case 'S':
+          // console.log("swords found");
+          return 1;
+        
+        case 'W':
+          // console.log("wands found");
+          return 1;
+        
+        default:
+          return 0;
+      }
+    },
+
+    getMDeltaFromSuit: (suit) => {
+      
+      switch (suit) {      
+
+        case 'D':
+          // console.log("disks found");
+          return -1;
+       
+        case 'C':
+          // console.log("cups found");
+          return 1;
+       
+        case 'S':
+          // console.log("swords found");
+          return 1;
+        
+        case 'W':
+          // console.log("wands found");
+          return -1;
+        
+        default:
+          return 0;
+      }
+    }
+  });
+
+  const newSelf = Object.assign(
+            self, 
+            deckCapabilities(self)
+          );
+
+  return newSelf;
+}
+
+module.exports = DeckUtils;
---
+++ src/DeckUtils.test.js
@@ -0,0 +1,69 @@
+const DeckUtils = require('./DeckUtils');
+const testUtils = DeckUtils();
+
+
+it('should have at least one test', () => {
+	expect(true).toBe(true);
+});
+
+describe('scenarios', () => {
+
+	const expectedScenarios = 
+		[
+			[0, '4D', '5W', '3C', '2S'],
+			[1, '6D', '5S', '3D', '2W'],
+			[2, '9D', '6W', '8C', '4S'],
+			[3, '5C', '8D', '3S', '2D'],
+			[4, '7D', '5D', '3W', '10S'],
+			[5, '7W', '8W', '6S', '9S'],
+			[6, '7C', '10D', '9W', '2C'],
+			[7, '10W', '10C', '9C', '4C'],
+			[8, '8S', '7S', '4W', '6C']
+		];
+
+	test.each(expectedScenarios)(
+		"given scenario index %p, returns four cards, %p, %p, %p, and %p",
+		(scenarioIndex, daemonCard, playerCardOne, playerCardTwo, playerCardThree) => {
+
+			testUtils.getScenarios();			
+		}
+	);
+});
+
+describe('delta utils', () => {
+
+	const suitToHDeltaCases = 
+		[
+			['W', 1], 
+			['C', -1], 
+			['S', 1], 
+			['D', -1], 
+			['?', 0]
+		];
+
+	test.each(suitToHDeltaCases)(
+		"given suit %p as arg, returns HDelta of %p",
+		(suitArg, expectedResult) => {
+			const result  = testUtils.getHDeltaFromSuit(suitArg);
+			expect(result).toEqual(expectedResult);
+		}
+	);
+
+	const suitToMDeltaCases = 
+		[
+			['W', -1], 
+			['C', 1], 
+			['S', 1], 
+			['D', -1], 
+			['?', 0]
+		];
+
+	test.each(suitToMDeltaCases)(
+		"given suit %p as arg, returns MDelta of %p",
+		(suitArg, expectedResult) => {
+			const result  = testUtils.getMDeltaFromSuit(suitArg);
+			expect(result).toEqual(expectedResult);
+		}
+	);
+});
+
